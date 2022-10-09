71af041fe99cf391afa096732b38c39a7d34d40a
Chloe Kuo <cmkuo@usc.edu>
2022-10-09 11:14:25 -0700

    Added spinner to managementorships loading

--- src/components/ManageMentorships.js
+++ src/components/ManageMentorships.js
@@ -1,61 +1,99 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/ManageMentorships.css";
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid } from "@mui/x-data-grid";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const columns = [
-  { field: "mentorName",
+  {
+    field: "mentorName",
     headerName: "Mentor",
     flex: 1,
     align: "center",
     headerAlign: "center",
     headerClassName: "table-header"
   },
-  { field: "menteeName",
+  {
+    field: "menteeName",
     headerName: "Mentees",
     flex: 1,
     align: "center",
     headerAlign: "center",
-    headerClassName: "table-header" }
-];
-
-const rows = [
-  {
-    id: 1,
-    mentorName: "Uma Durairaj",
-    menteeName: "Student A",
-  },
-  {
-    id: 2,
-    mentorName: "Chloe Kuo",
-    menteeName: "Student B",
-  },
-  {
-    id: 3,
-    mentorName: "Ayushi Mittal",
-    menteeName: "Student C",
-  },
-  {
-    id: 4,
-    mentorName: "Erica De Guzman",
-    menteeName: "Student D",
+    headerClassName: "table-header"
   }
 ];
 
 const ManageMentorships = () => {
+  const [rows, setRows] = useState([]);
+  const [initalized, setinitalized] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const menteeToMentor = new Map();
+  let id = 0;
+  useEffect(() => {
+    const fetchMentors = async () => {
+      const mentors = await fetch(
+        `https://progress-reports-portal-node.herokuapp.com/select_table?table_name=mentor_info`
+      );
+      const mentorsJson = await mentors.json();
+      return mentorsJson;
+    };
+
+    const fetchMenteesFromMentors = async (mentorId) => {
+      const mentees = await fetch(
+        `https://progress-reports-portal-node.herokuapp.com/get_mentees_of_mentor_id?id=${mentorId}`
+      );
+      const menteesJson = await mentees.json();
+      return menteesJson;
+    };
+
+    if (!initalized) {
+      setLoading(true);
+      fetchMentors().then((mentors) => {
+        mentors.forEach((mentor) => {
+          fetchMenteesFromMentors(mentor.id).then((mentees) => {
+            if (mentees.length > 0) {
+              mentees.forEach((mentee) => {
+                // check if mentor already exists in table
+                if (!menteeToMentor.has(mentee.name)) {
+                  menteeToMentor.set(mentee.name, mentor.name);
+                  // add new mentee / mentor row to table
+                  setRows((rows) => [
+                    ...rows,
+                    {
+                      id: id++,
+                      mentorName: `${mentor.name}`,
+                      menteeName: `${mentee.name}`
+                    }
+                  ]);
+                }
+              });
+            }
+            setinitalized(true);
+            setLoading(false);
+          });
+        });
+      });
+    }
+  }, []);
+
   return (
     <div>
-    <div id="manage-mentorships">
-      <h1>Manage Mentorships</h1>
-      <div style={{ height: 525, width: "100%" }}>
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            pageSize={5}
-            rowsPerPageOptions={[5]}
-          />
+      <div id="manage-mentorships">
+        <h1>Manage Mentorships</h1>
+        <div style={{ height: 525, width: "100%" }}>
+          {loading ? (
+            <ClipLoader color={"#123abc"} loading={loading} size={50} />
+          ) : (
+              <DataGrid
+              rows={rows}
+              columns={columns}
+              pageSize={5}
+              rowsPerPageOptions={[5]}
+            />
+          )}
+
         </div>
+      </div>
     </div>
-  </div>
   );
 };
 
