c0827ba0f06b8e680df5bd66a60ed6bac058c9b1
Chloe Kuo <cmkuo@usc.edu>
2022-10-14 19:42:52 -0700

    progress report template fetches data

--- src/components/ProgressReportTemplate.js
+++ src/components/ProgressReportTemplate.js
@@ -1,13 +1,57 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import "../styles/ProgressReportTemplate.css";
 import ProgressReportTemplateQuestionForm from "./ProgressReportTemplateQuestionForm";
 
 const ProgressReportTemplate = () => {
+
+  const [initalized, setinitalized] = useState(false);
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    const fetchCurrentQuestionOrder = async () => {
+      const current_question_order = await fetch(
+        `https://progress-reports-portal-node.herokuapp.com/get_current_question_order`
+      );
+      const currentQuestionOrderJson = await current_question_order.json();
+      return currentQuestionOrderJson;
+    };
+
+    const fetchQuestionFromId = async (questionId) => {
+      const current_question_order = await fetch(
+        `https://progress-reports-portal-node.herokuapp.com/get_question_by_id?id=${questionId}`
+      );
+      const currentQuestionOrderJson = await current_question_order.json();
+      return currentQuestionOrderJson;
+    };
+    
+
+    if(!initalized) {
+      fetchCurrentQuestionOrder().then((questionIds) => {
+        (questionIds.question_order).forEach((questionId) => {
+          fetchQuestionFromId(questionId).then((question) => {
+            if(questions.length < questionIds.question_order.length) {
+              questions.push(question);
+              if(questions.length == questionIds.question_order.length) {
+                setinitalized(true);
+              }
+            }
+          })
+        })
+      })
+    }
+
+  }, [initalized]);
+
   return (
     <div className="progress-report-template">
       <h1>Progress Report Template</h1>
-      <ProgressReportTemplateQuestionForm question={""} />
-      <div> potato</div>
+      <div>
+        {questions.map((question) => (
+          <ProgressReportTemplateQuestionForm key={question.id} question={question} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/ProgressReportTemplateQuestionForm.js
+++ src/components/ProgressReportTemplateQuestionForm.js
@@ -1,32 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/ProgressReportTemplate.css";
 
 const ProgressReportTemplateQuestionForm = ({ question }) => {
-  const [toggle, setToggle] = React.useState(false);
+  const [toggle, setToggle] = useState(false);
+
+  const [selected, setSelected] = useState(question.type);
+
+  const handleChange = (event) => {
+    setSelected(event.target.value);
+  };
 
   const showDropdown = (e) => {
     setToggle(!toggle);
   };
+
   return (
     <div className="dropdown">
       <button onClick={showDropdown} className="dropbtn">
-        Dropdown
+        {question.question}
       </button>
       <div
         style={{ display: toggle ? "block" : "none" }}
         className="dropdown-content"
       >
-      <div class="container">
-        <form>
-          <label>First Name</label>
-          <input type="text" name="first"/><br />
-          <label>Last Name</label>
-          <input type="text" name="last"/><br />
-          <label>Email</label>
-          <input type="text" name="email"/><br />
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
+        <div className="form-input-container">
+          <form>
+            <label className="question-label">Question Title:</label>
+            <input
+              className="question-input"
+              type="text"
+              name="question_title"
+              defaultValue={question.question}
+            />
+            <br />
+            <label className="question-label">Description:</label>
+            <input
+              className="question-input"
+              type="text"
+              name="question_description"
+              defaultValue={question.description}
+            />
+            <br />
+            <label className="question-label">Question Type:</label>
+            <label className="radio-inline">
+              <input
+                type="radio"
+                name="optradio"
+                value="Multiple choice"
+                checked={selected == "Multiple choice"}
+                onChange={handleChange}
+              />
+              Multiple Choice
+            </label>
+            <label className="radio-inline">
+              <input
+                type="radio"
+                name="optradio"
+                value="Short answer"
+                checked={selected == "Short answer"}
+                onChange={handleChange}
+              />
+              Short Answer
+            </label>
+            {selected == "Multiple choice" ?
+            (<div>
+
+            </div>) : (<div></div>)}
+          </form>
+        </div>
       </div>
     </div>
   );
